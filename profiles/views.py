4312953ade0e11389811526743735397bf0769e4
Shobin23 <shobin23@gmail.com>
2016-12-17 20:03:03 +0530

    Test Server

--- profiles/views.py
+++ profiles/views.py
@@ -5,19 +5,18 @@ from django.http import Http404
 
 
 def index(request):
-	all_task=Datamain.objects.all()
-	return render(request,'profiles/index.html',{'all_task': all_task})
-	
-	''' This is a new test'''
-	'''for album in all_task:
+    all_task = Datamain.objects.all()
+    return render(request, 'profiles/index.html', {'all_task': all_task})
+
+    '''for album in all_task:
 		url='/home/' + str(album.id)
 		html += '<a href="' + url + '">' + album.main_task+'</a><br>'''
 
-def detail(request,task_details):
-	#return HttpResponse("<h2>Details For Task:"+str(task_details)+ "</h2>")
-	try:		
-		task = Datamain.objects.get(pk=task_details)
-	except Datamain.DoesNotExist:
-		raise Http404("Task does not exist")
-	return render(request,'profiles/details.html',{'task': task})	
-			
+
+def detail(request, task_details):
+    # return HttpResponse("<h2>Details For Task:"+str(task_details)+ "</h2>")
+    try:
+        task = Datamain.objects.get(pk=task_details)
+    except Datamain.DoesNotExist:
+        raise Http404("Task does not exist")
+    return render(request, 'profiles/details.html', {'task': task})
